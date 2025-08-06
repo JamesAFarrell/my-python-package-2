aca5e3dbebf671fda40010a8f09d016e5ce8cf52
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2025-08-06 14:33:50 +0100

    fix: add __all__

--- src/hds_functions/__init__.py
+++ src/hds_functions/__init__.py
@@ -1,3 +1,8 @@
 """Package entry point and version."""
 
 from .data_privacy import redact_low_counts, round_counts_to_multiple
+
+__all__ = [
+    "redact_low_counts",
+    "round_counts_to_multiple",
+]
